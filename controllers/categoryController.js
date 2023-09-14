ec4ec51f2eaa081486b423cd29a4834ca8709bc0
Maninder Pahal <mpahal123@gmail.com>
2023-09-13 21:57:25 -0700

    completed app

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const { isObjectIdOrHexString } = require("mongoose");
 const Category = require("../models/category");
 const Item = require("../models/item");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
     const make = await Category.find().exec();
@@ -23,25 +23,111 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create GET");
+    res.render("category_form", { title: "Add make" });
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create POST");
-});
+exports.category_create_post = [
+    body("name", "Name must be atleast 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body("description", "Description required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+        asyncHandler(async (req, res, next) => {
+            const errors = validationResult(req);
+            const make = new Category({ 
+                name: req.body.name,
+                description: req.body.description,
+            });
+
+            if (!errors.isEmpty()) {
+                res.render("category_form", { title: "Add Make", make, errors: errors.array()});
+                return;
+            } else {
+                const makeExists = await Category.findOne({ name: req.body.name }).exec();
+                if (makeExists) {
+                    res.redirect(makeExists.url);
+                } else {
+                    await make.save();
+                    res.redirect(make.url);
+                }
+            };
+        })
+]
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category delete GET");
+    const [bike, make] = await Promise.all([
+        Item.find({ category: req.params.id}).exec(),
+        Category.findById(req.params.id).exec(),
+    ]);
+
+    if (make === null) {
+        res.redirect('/catalog/categorys');
+    }
+
+    res.render("category_delete", {
+        title: "Delete Make",
+        makers: make,
+        bikes: bike
+    })
 });
 
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: category delete GET");
+  const [bike, make] = await Promise.all([
+    Item.find({ category: req.params.id}).exec(),
+    Category.findById(req.params.id).exec()
+  ]);
+
+  if (bike.length > 0) {
+    res.render("item_delete", {
+        title: "Delete bike",
+        makers: make,
+        bikes: bike
+    })
+    return;
+  } else {
+    await Category.findByIdAndRemove(req.params.id);
+    res.redirect("/catalog/categorys");
+  }
 });
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create GET");
+    const make = await Category.findById(req.params.id).exec();
+    res.render("category_form", { 
+        title: "Update Make",
+        makers: make
+    })
 });
 
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create GET");
-});
+exports.category_update_post = [
+    body("name", "Name must be atleast 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body("description", "Description required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const make = new Category({ 
+            name: req.body.name,
+            description: req.body.description,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            const make = await Category.findById(req.params.id).exec();
+            res.render("category_form", {
+                title: "Update make",
+                makers: make
+            })
+        } else {
+            const updatedCategory = await Category.findByIdAndUpdate(req.params.id, make, {});
+            res.redirect(updatedCategory.url);
+        }
+    })
+];
+
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.item_index = asyncHandler(async (req, res, next) => {
     res.render('index', { title: 'Super Sport Inventory'});
@@ -17,25 +18,136 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create GET");
+    const make = await Category.find().exec();
+    res.render("item_form", { title: "Add bike", maker: make});
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create POST");
-});
+exports.item_create_post = [
+    body("name", "Name must be atleast 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body("price", "Price is required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("quantity", "Number required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("description", "Description required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("category").escape(),
+        asyncHandler(async (req, res, next) => {
+            const errors = validationResult(req);
+            console.log(req.body.category);
+            const bike = new Item({ 
+                name: req.body.name,
+                category: req.body.category,
+                price: req.body.price,
+                description: req.body.description,
+                quantity: req.body.quantity
+            });
+
+            if (!errors.isEmpty()) {
+                res.render("item_form", { title: "Add bike", bike, errors: errors.array()});
+                return;
+            } else {
+                const bikeExists = await Item.findOne({ name: req.body.name }).exec();
+                if (bikeExists) {
+                    res.redirect(bikeExists.url);
+                } else {
+                    await bike.save();
+                    res.redirect(bike.url);
+                }
+            };
+        })
+]
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item delete GET");
+    const bikes = await Item.findById(req.params.id).populate('category').exec();
+    if(bikes === null) {
+        res.redirect("/catelog/items");
+    }
+
+    res.render("item_delete", {
+        title: "Delete bike",
+        bike: bikes
+    })
 });
 
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: item delete GET");
+  await Item.findByIdAndRemove(req.params.id);
+  res.redirect("/catalog/items");
 });
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create GET");
+    const [bikey, make] = await Promise.all([
+        Item.findById(req.params.id).populate('category').exec(),
+        Category.find().exec()
+    ]);
+
+    if (bikey === null) {
+        const err = new Error("Bike not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("item_form", {
+        title: "Add bike",
+        bike: bikey,
+        maker: make
+    })
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create GET");
-});
+exports.item_update_post = [
+    body("name", "Name must be atleast 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body("price", "Price is required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("quantity", "Number required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("description", "Description required")
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body("category").escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const bike = new Item({
+            name: req.body.name,
+            category: req.body.category,
+            price: req.body.price,
+            description: req.body.description,
+            quantity: req.body.quantity,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            const [bikey, make] = await Promise.all([
+                Item.findById(req.params.id).populate("category").exec(),
+                Category.find().exec()
+            ]);
+            console.log(bikey);
+            res.render("item_form", {
+                title: "Add bike",
+                bike: bikey,
+                maker: make
+            });
+            return;
+        } else {
+            const updatedBike = await Item.findByIdAndUpdate(req.params.id, bike, {});
+            res.redirect(updatedBike.url);
+        }
+    })
+]
