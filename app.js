adaa8bccc1553757536e8bb9f2cb8324043ee156
Maninder Pahal <mpahal123@gmail.com>
2023-09-16 23:38:04 -0700

    changed dot env import statement from es6 to node type require

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const catalogRouter = require('./routes/catalog');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+require('dotenv').config();
 
 var app = express();
 
